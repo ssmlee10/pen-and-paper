45e55f68fcd7a0bc6ee92bc65c4597aefac0c882
Sharon Lee <ssmlee10@gmail.com>
2025-09-15 13:19:45 -0700

    connect css

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
