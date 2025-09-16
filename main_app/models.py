7f057ad21476e97d524dc36ef29016acd50e3a0c
Sharon Lee <ssmlee10@gmail.com>
2025-09-16 02:05:49 -0700

    create templates: pens index, about, home, base, create pen model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+NIB_SIZE = (
+  ('EF', 'Extra Fine'),
+  ('F', 'Fine'),
+  ('M', 'Medium'),
+  ('B', 'Bold'),
+)
+
+NIB_MATERIAL = (
+  ('S', 'Steel'),
+  ('G14', 'Gold 14k'),
+  ('G18', 'Gold 18k'),
+  ('G21', 'Gold 21k'),
+  ('T', 'Titanium'),
+  ('P', 'Platinum'),
+  ('B', 'Bronze'),
+  ('G', 'Glass'),
+)
+
+INK_FILL_MECHANISM = (
+   ('CA', 'Cartridge'),
+   ('CO', 'Cartridge-Converter'),
+   ('P', 'Piston-Fill'),
+   ('V', 'Vacuum-Fill'),
+)
+
+class Pen(models.Model): 
+  brand = models.CharField(max_length=100)
+  model = models.CharField(max_length=100)
+  nib_size = models.CharField(
+    max_length = 2,
+    choices = NIB_SIZE,
+    default = NIB_SIZE[0][0]
+  )
+  nib_material = models.CharField(
+    max_length = 3,
+    choices = NIB_MATERIAL,
+    default = NIB_MATERIAL[0][0]
+  )
+  ink_fill = models.CharField(
+    max_length = 2,
+    choices = INK_FILL_MECHANISM,
+    default = INK_FILL_MECHANISM[0][0]
+  )
+  acquired_date = models.DateField(blank=True, null=True)
+  notes = models.TextField(blank=True, null=True)
+
+  def __str__(self):
+    return f"{self.brand} {self.model} ({self.nib_size})"
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# views.py
+
+class Pen:
+    def __init__(self, brand, name):
+        self.brand = brand
+        self.name = name
+
+# Create a list of Cat instances
+pens = [
+    Pen('Lamy', 'Lamy 2000'),
+    Pen('Radius', 'Marmo Bianco'),
+    Pen('Pilot', 'Custom 823'),
+]
+
 def home(request):
-  return render(request, 'home.html')
+  return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def pen_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'pens/index.html', {'pens': pens})
