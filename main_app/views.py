cadf5450e711363c83bddc4c5cac12b7b626f114
Sharon Lee <ssmlee10@gmail.com>
2025-09-16 12:24:06 -0700

    signup user function complete

--- main_app/views.py
+++ main_app/views.py
@@ -42,15 +42,15 @@ class PenDelete(LoginRequiredMixin, DeleteView):
     success_url = '/pens/'
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('cat-index')
-    else:
-        error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('pen-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
