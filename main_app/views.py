87054aabd4ced4f1a458618b2a242cdf0d5eeac8
Sharon Lee <ssmlee10@gmail.com>
2025-09-16 12:02:52 -0700

    protect routes when not logged in

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # views.py
@@ -14,15 +16,16 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def pen_index(request):
-  pens = Pen.objects.all()
+  pens = Pen.objects.filter(user=request.user)
   return render(request, 'pens/index.html', {'pens': pens})
 
 def pen_detail(request, pen_id):
   pen = Pen.objects.get(id=pen_id)
   return render(request, 'pens/detail.html', {'pen': pen})
 
-class PenCreate(CreateView):
+class PenCreate(LoginRequiredMixin, CreateView):
   model = Pen
   fields = '__all__'
 
@@ -30,11 +33,11 @@ def form_valid(self, form):
   form.instance.user = self.request.user
   return super().form_valid(form)
 
-class PenUpdate(UpdateView):
+class PenUpdate(LoginRequiredMixin, UpdateView):
   model = Pen
   fields = ['nib_size', 'nib_material', 'ink_fill', 'acquired_date', 'notes']
 
-class PenDelete(DeleteView):
+class PenDelete(LoginRequiredMixin, DeleteView):
     model = Pen
     success_url = '/pens/'
 
